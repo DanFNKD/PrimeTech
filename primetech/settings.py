33654550f4817bbc5078201fd328edd7392d5e91
Daniel Harris <dwh261@gmail.com>
2025-02-02 11:31:44 +0000

    Set debug status for deployment

--- primetech/settings.py
+++ primetech/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'fallback-secret-key')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = [
     '8000-danfnkd-primetech-m3w2o7c6p32.ws.codeinstitute-ide.net',
