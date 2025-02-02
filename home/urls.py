4c8111f068be66e458c4c83b19f7415cb32615c0
Daniel Harris <dwh261@gmail.com>
2025-02-02 14:54:41 +0000

    Add temporary superuser creation view

--- home/urls.py
+++ home/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('create-admin/', views.create_admin_user, name='create_admin'), 
 ]
