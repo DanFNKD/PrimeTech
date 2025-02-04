27383a7b2901170bea4ce500d127ce30d31316c1
Daniel Harris <dwh261@gmail.com>
2025-02-04 00:11:54 +0000

    Updated checkout signals and apps for emails

--- checkout/signals.py
+++ checkout/signals.py
@@ -1,20 +1,28 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
-
-from .models import OrderLineItem
-
+from django.core.mail import send_mail
+from django.conf import settings
+from .models import OrderLineItem, Order
 
 @receiver(post_save, sender=OrderLineItem)
 def update_on_save(sender, instance, created, **kwargs):
-    """
-    Update order total on lineitem update/create
-    """
     instance.order.update_total()
-
 
 @receiver(post_delete, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwargs):
-    """
-    Update order total on lineitem delete
-    """
+def update_on_delete(sender, instance, **kwargs):
     instance.order.update_total()
+
+@receiver(post_save, sender=Order)
+def send_order_confirmation_email(sender, instance, created, **kwargs):
+    if created:
+        subject = f"Order Confirmation - {instance.order_number}"
+        message = f"Thank you for your order! Your order number is {instance.order_number}."
+        recipient = instance.email
+
+        send_mail(
+            subject,
+            message,
+            settings.DEFAULT_FROM_EMAIL,
+            [recipient],
+            fail_silently=False,
+        )
