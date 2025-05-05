8c2c9537f3e3c37e869e16358f9a12a5a3d3b033
DanFNKD <dwh261@gmail.com>
2025-05-05 18:00:19 +0100

    feat(mailchimp): integrate MailChimp signup for newsletter subscription

--- primetech/settings.py
+++ primetech/settings.py
@@ -193,3 +193,6 @@ else:
     EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
     DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+
+MAILCHIMP_API_KEY = os.environ.get('MAILCHIMP_API_KEY', '')
+MAILCHIMP_LIST_ID = os.environ.get('MAILCHIMP_LIST_ID', '')
