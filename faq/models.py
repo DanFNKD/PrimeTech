581f7ec25d795b08074d93e07a7f9a03ec5810bb
DanFNKD <dwh261@gmail.com>
2025-05-11 08:20:59 +0100

    style(faqs): update python files for PEP8 compliance

--- faq/models.py
+++ faq/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils.timezone import now
 from django.urls import reverse
 
+
 class FAQ(models.Model):
     question = models.CharField(max_length=255)
     answer = models.TextField()
